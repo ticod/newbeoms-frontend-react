a6e2e67b995a00f9fa7ce06facc1cf12548b761c
tico <lours2021@gmail.com>
2024-07-07 04:55:06 +0900

    feat update NewsImage

--- src/components/news/NewsImage.jsx
+++ src/components/news/NewsImage.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
 import styles from "./NewsImage.module.css";
 
 function NewsImage(props) {
+  const { pathname } = useLocation();
   const { image } = props;
+
+  const imagePath = pathname.startsWith('/publication') 
+    ? `${process.env.PUBLIC_URL}${image}` 
+    : `${process.env.PUBLIC_URL}publication/${image}`;
 
   return (
     <div className={styles.wrapper}>
-      <img className={styles.newsImage} src={image} alt=""/>
+      <img className={styles.newsImage} src={imagePath} alt=""/>
     </div>
   );
 }
